89be324dba2dc0849fb5600b283d96e174591050
Susith Hemathilaka <susith.h@directfn.com>
2020-09-04 14:20:50 +0530

    Layouts hbd

--- app.js
+++ app.js
@@ -6,12 +6,32 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var exphbs = require('express-handlebars');
+const { extname, dirname } = require('path');
 
 var app = express();
 
 // view engine setup
+
+
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
+
+
+// app.engine('hbs', hbs({extname: 'hbs', defaultLayout:'layout',layoutsDir: dirname+'/views/layouts/'}));
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'handlebs');
+
+// app.engine('hbs', exphbs({ defaultLayout: 'layout', extname: '.hbs' }));
+
+// app.set('view engine', 'handlebars');
+// app.set('views', path.join(__dirname, 'views/'));
+
+// app.engine('hbs', exphbs({defaultLayout: 'layout', extname: '.hbs'}));
+// app.set('views', path.join(__dirname, 'views/'));
+// app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,17 +43,17 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  //render the error page
   res.status(err.status || 500);
   res.render('error');
 });
